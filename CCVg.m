4e4f4a78a21a8f57b5e028105da29e92d7d72f22
181069 <72987407+181069@users.noreply.github.com>
2021-11-15 13:31:25 +0200

    .

--- CCVg.m
+++ CCVg.m
@@ -1,13 +1,16 @@
 
 
 
+
 function [CVR, CVG, CVB]= CCVg(rgbIMG,numOfColors,threshold)
 
 [Rb,Gb,Bb] = BlurAndDivideChannels(rgbIMG,3);
 [RChannel] = discretizeColors(Rb,numOfColors);
 [GChannel] = discretizeColors(Gb,numOfColors);
 [BChannel] = discretizeColors(Bb,numOfColors);
-CVR =CCV( RChannel , threshold);
+Afterblur = cat(2,RChannel,GChannel,BChannel);
+imshow(Afterblur);
+CVR =CCV( Afterblur , threshold);
 CVG=CCV( GChannel , threshold);
 CVB=CCV( BChannel , threshold);
 end
